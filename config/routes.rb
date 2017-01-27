72a32a151a67acb5fbb547f12876153797d2ef4c
nnikolaev <nnikolayew@gmail.com>
2017-01-27 20:04:07 +0300

    adadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-  get 'library/index'
+  resources :libraries
+  get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root to: 'library#index', as: 'library'
+    root to: 'home#index', as: 'home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
