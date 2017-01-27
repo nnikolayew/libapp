2e3028284ef79c6226d1d75ae4df794c43cf08d0
nnikolaev <nnikolayew@gmail.com>
2017-01-27 20:18:19 +0300

    adadas

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-    root to: 'libraries#index'
+    root to: 'index', as: 'libraries'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
