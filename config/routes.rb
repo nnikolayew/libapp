afa7a3ef6921b5b247871478bd30721698e97bbe
nnikolaev <nnikolayew@gmail.com>
2017-01-27 20:34:18 +0300

    adadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   resources :products
-  get 'home/index'
     get 'libraries/index'
+  get 'home/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
